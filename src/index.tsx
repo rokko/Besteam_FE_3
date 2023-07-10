d36df173ff8292dfcf60227925c7046d640ba4d7
Rocco Caricola <rocco.caricola89@gmail.com>
2023-07-10 16:07:00 +0200

    aggionramento versione node

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import {Provider} from "react-redux";
-import {store} from "../src/components/redux/store";
-import App from './App';
-document.title="Besteam.io"
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "../src/components/redux/store";
+import App from "./App";
+import "./polyfill.js";
+document.title = "Besteam.io";
 
 ReactDOM.render(
-  
-
   <React.StrictMode>
-      <Provider store={store}>
-    <App />
-      </Provider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-

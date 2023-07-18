4448110d4fe9a05c54ae58a9e98b50f260917039
Rocco Caricola <rocco.caricola@alten.it>
2023-07-18 17:58:23 +0200

    correzione footer

--- src/Screens/btemSwap/boxDescrizione/BoxDescrizione.tsx
+++ src/Screens/btemSwap/boxDescrizione/BoxDescrizione.tsx
@@ -149,8 +149,9 @@ const BoxDescrizione = () => {
           }}
         >
           <p>
-            Besteam Game S.R.L. © 2022, All rights reserved - VAT 16643031004,
-            Roma
+          Besteam Game S.R.L. ©️ 2022, All rights reserved <br />
+          Via Roccella Jonica 25, 00173 Roma - VAT 16643031004 -
+          besteamgamesrl@legalmail.it
           </p>
         </div>
       </ContainerDescrizione>

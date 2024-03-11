c3c4137ca2364238dbddebf500f361a0710e4027
roccocaricoladev <161312616+roccocaricoladev@users.noreply.github.com>
2024-03-11 15:33:16 +0100

    inizio costruzione create club

--- src/dappNew/CreazioneClub/CreazioneClub.tsx
+++ src/dappNew/CreazioneClub/CreazioneClub.tsx
@@ -33,6 +33,8 @@ const CreazioneClub = () => {
   const [stadiumName, setStadiumName] = useState("")
   const [metaverseZone, setMetaverseZone] = useState("")
   const [start, setStart] = useState(false)
+  const [firstStep, setFirstStep] = useState(false)
+  const [secondStep, setSecondstep] = useState(true)
 
 
 
@@ -61,70 +63,83 @@ const CreazioneClub = () => {
   }
 
   if (!!start) return (
+
     <div style={{ backgroundColor: '#1C1C1C', display: 'flex', flexDirection: 'column', height: '100%' }}>
       <HeaderDapp />
       <LeftMenu />
 
-      <div style={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'row', position: 'fixed', top: '4.167vw', left: ' 5vw', gap: '3.125vw', backgroundColor: '#1C1C1C', alignContent: 'center', alignItems: 'center', justifyContent: 'center' }}>
-        <div style={{ width: '40.069vw', height: '37.5vw', display: 'flex', flexDirection: 'column', backgroundColor: '#141414', padding: '4.514vw' }}>
-          <TestoLeftInput>Logo</TestoLeftInput>
-          <div style={{ width: '31.042vw', height: '15.486vw', backgroundColor: 'white' }}>
+      {(!!firstStep) &&
+         <div style={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'row', position: 'fixed', top: '4.167vw', left: ' 5vw', gap: '3.125vw', backgroundColor: '#1C1C1C', alignContent: 'center', alignItems: 'center', justifyContent: 'center' }}>
+         <div style={{ width: '40.069vw', height: '37.5vw', display: 'flex', flexDirection: 'column', backgroundColor: '#141414', padding: '4.514vw' }}>
+           <TestoLeftInput>Logo</TestoLeftInput>
+           <div style={{ width: '31.042vw', height: '15.486vw', backgroundColor: 'white' }}>
+ 
+           </div>
+           <TestoLeftInput>Shape</TestoLeftInput>
+           <div style={{ width: '31.042vw', height: '6.215vw', backgroundColor: 'white' }}>
+ 
+           </div>
+           <div style={{ display: 'flex', flexDirection: 'row' }}>
+             <TestoLeftInput>Main color</TestoLeftInput>
+             <TestoLeftInput>Secondary color</TestoLeftInput>
+           </div>
+ 
+         </div>
+         <div style={{ width: '40.069vw', height: '37.5vw', display: 'flex', flexDirection: 'column', backgroundColor: '#141414', justifyContent: 'center', alignContent: 'center' , alignItems:'center',gap:'1.217vw'}}>
+ 
+ 
+           <div style={{ display: 'flex', flexDirection: 'row', gap: '1.5vw', justifyContent: 'center'}}>
+             <div style={{display:'flex', flexDirection:'column', gap :'0.519vw'}}>
+               <TestoLeftInput>Team name</TestoLeftInput>
+               <InputClub style={{ width: '21.875vw' }} onChange={(e) => setName(e.target.value)} />
+             </div>
+             <div>
+               <TestoLeftInput>abbreviation</TestoLeftInput>
+               <InputClub style={{ width: '12.135vw' }} onChange={(e) => setAbbreviation(e.target.value)} />
+             </div>
+ 
+           </div>
+           <TestoLeftInput>Clubs Name</TestoLeftInput>
+           <InputClub onChange={(e) => setClubsName(e.target.value)} />
+           <div><TestoLeftInput>HUB Name</TestoLeftInput>
+ 
+             <InputClub onChange={(e) => setStadiumName(e.target.value)} />
+           </div>
+ 
+           <div>
+             <TestoLeftInput>Stadium Name</TestoLeftInput>
+ 
+             <InputClub onChange={(e) => setTwitterPage(e.target.value)} />
+           </div>
+           <div>
+ 
+             <div>
+               <TestoLeftInput>Main Social Page</TestoLeftInput>
+               <InputClub onChange={(e) => setLivePage(e.target.value)} />
+             </div>
+ 
+ 
+ 
+             <div><TestoLeftInput>Official Live Page</TestoLeftInput>
+ 
+               <InputClub onChange={(e) => setMetaverseZone(e.target.value)} />
+             </div>
+ 
+ 
+           </div>
+         </div>
+       </div>}
 
-          </div>
-          <TestoLeftInput>Shape</TestoLeftInput>
-          <div style={{ width: '31.042vw', height: '6.215vw', backgroundColor: 'white' }}>
-
-          </div>
-          <div style={{ display: 'flex', flexDirection: 'row' }}>
-            <TestoLeftInput>Main color</TestoLeftInput>
-            <TestoLeftInput>Secondary color</TestoLeftInput>
-          </div>
-
-        </div>
-        <div style={{ width: '40.069vw', height: '37.5vw', display: 'flex', flexDirection: 'column', backgroundColor: '#141414', justifyContent: 'center', alignContent: 'center' , alignItems:'center',gap:'1.217vw'}}>
-
-
-          <div style={{ display: 'flex', flexDirection: 'row', gap: '1.5vw', justifyContent: 'center'}}>
-            <div style={{display:'flex', flexDirection:'column', gap :'0.519vw'}}>
-              <TestoLeftInput>Team name</TestoLeftInput>
-              <InputClub style={{ width: '21.875vw' }} onChange={(e) => setName(e.target.value)} />
-            </div>
-            <div>
-              <TestoLeftInput>abbreviation</TestoLeftInput>
-              <InputClub style={{ width: '12.135vw' }} onChange={(e) => setAbbreviation(e.target.value)} />
-            </div>
-
-          </div>
-          <TestoLeftInput>Clubs Name</TestoLeftInput>
-          <InputClub onChange={(e) => setClubsName(e.target.value)} />
-          <div><TestoLeftInput>HUB Name</TestoLeftInput>
-
-            <InputClub onChange={(e) => setStadiumName(e.target.value)} />
-          </div>
-
-          <div>
-            <TestoLeftInput>Stadium Name</TestoLeftInput>
-
-            <InputClub onChange={(e) => setTwitterPage(e.target.value)} />
-          </div>
-          <div>
-
-            <div>
-              <TestoLeftInput>Main Social Page</TestoLeftInput>
-              <InputClub onChange={(e) => setLivePage(e.target.value)} />
-            </div>
-
-
-
-            <div><TestoLeftInput>Official Live Page</TestoLeftInput>
-
-              <InputClub onChange={(e) => setMetaverseZone(e.target.value)} />
-            </div>
-
-
-          </div>
-        </div>
+      {(!!secondStep) &&
+         <div style={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'row', position: 'fixed', top: '4.167vw', left: ' 5vw', gap: '3.125vw', backgroundColor: '#1C1C1C', alignContent: 'center', alignItems: 'center', justifyContent: 'center' }}>
+      <div style={{width:'83.33vw',height:'37.5vw', backgroundColor:'#141414'}}></div>
       </div>
+      }
+
+
+
+      
+     
 
 
     </div>

206b3375af62f368c5694bf87b3b9760257565d0
Rocco Caricola <roccocaricola.dev@outlook.com>
2024-10-14 21:11:41 +0200

    corerzione

--- src/communityCup/VerifyPage.tsx
+++ src/communityCup/VerifyPage.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import communitybackground from './sfondoCommunity.jpeg'
 import { BloccoTesto, ButtonVerify, SpanTesto } from './VerifyPageStyled'
-import ButtonConnessione from './ButtonVerifyThirdWeb'
 
 
 

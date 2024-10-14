2671a780aa4bd28448cda480a10c095ba426e4ed
Rocco Caricola <roccocaricola.dev@outlook.com>
2024-10-14 17:41:43 +0200

    correzione verify page

--- src/communityCup/VerifyPage.tsx
+++ src/communityCup/VerifyPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import communitybackground from './sfondoCommunity.jpeg'
 import { BloccoTesto, ButtonVerify, SpanTesto } from './VerifyPageStyled'
 
@@ -8,11 +8,15 @@ import { BloccoTesto, ButtonVerify, SpanTesto } from './VerifyPageStyled'
 const VerifyPage = () => {
     const [verified, setVerified] = useState(false)
 
-    const verifyUser=()=>{
+    const verifyUser = () => {
         setVerified(true)
     }
+
+    useEffect(() => {
+        console.log(verified)
+    }, [verified])
     return (
-        <div 
+        <div
             style={{
                 backgroundImage: `url(${communitybackground})`,
                 backgroundSize: 'cover',
@@ -25,33 +29,34 @@ const VerifyPage = () => {
                 color: 'white' // Modifica il colore del testo se necessario
             }}
         >
-            <div style={{display:'flex',gap:'1.5rem', flexDirection:'column',marginTop:'70px' ,justifyContent:'center', alignContent:'center', alignItems:'center'
+            <div style={{
+                display: 'flex', gap: '1.5rem', flexDirection: 'column', marginTop: '70px', justifyContent: 'center', alignContent: 'center', alignItems: 'center'
             }}>
                 {!verified && (
                     <>
-                          <ButtonVerify onClick={verifyUser()}>
-                          <p>VERIFY</p>
-                          </ButtonVerify>
-                          <BloccoTesto>
-                              By clicking Verify, 
-              you accept the <SpanTesto>Privacy Policy</SpanTesto>
-                          </BloccoTesto>
-                          </>
+                        <ButtonVerify onClick={()=>verifyUser()}>
+                            <p>VERIFY</p>
+                        </ButtonVerify>
+                        <BloccoTesto>
+                            By clicking Verify,
+                            you accept the <SpanTesto>Privacy Policy</SpanTesto>
+                        </BloccoTesto>
+                    </>
 
                 )}
-                {verified &&(
-                          <>
-                          <ButtonVerify>
-                          <p>NEXT</p>
-                          </ButtonVerify>
-                           <SpanTesto>Verify completed,
-                           Join Community Cup!</SpanTesto>
-                          
-                          </>
-                    
+                {verified && (
+                    <>
+                        <ButtonVerify>
+                            <p>NEXT</p>
+                        </ButtonVerify>
+                        <SpanTesto>Verify completed<br/>
+                            Join Community Cup!</SpanTesto>
+
+                    </>
+
                 )}
 
-      
+
             </div>
 
 
